2467f5a524f1d4ece69b3d47ac5ac072fd78c42a
Nicolas Cisco <ncis20@gmail.com>
2019-10-08 23:16:50 -0300

    Added prettier

--- background.js
+++ background.js
@@ -1,67 +1,64 @@
 async function runCommand(command) {
-    const tabs = await browser.tabs.query({ url: "https://*.spotify.com/*" });
+  const tabs = await browser.tabs.query({ url: 'https://*.spotify.com/*' });
 
-    // Open a spotify tab if one does not exist yet.
-    if (tabs.length === 0) {
-        return;
+  // Open a spotify tab if one does not exist yet.
+  if (tabs.length === 0) {
+    return;
+  }
+
+  // Apply command
+  for (const tab of tabs) {
+    let code = '';
+
+    if (tab.url.startsWith('https://play.spotify.com')) {
+      code = `document.getElementById('app-player').contentDocument.getElementById('${command}').click()`;
+    } else if (tab.url.startsWith('https://open.spotify.com')) {
+      switch (command) {
+        case 'play-pause':
+          code =
+            "(document.querySelector('.spoticon-play-16') || document.querySelector('.spoticon-pause-16')).click()";
+          break;
+
+        case 'next':
+          code = "document.querySelector('.spoticon-skip-forward-16').click()";
+          break;
+
+        case 'previous':
+          code = "document.querySelector('.spoticon-skip-back-16').click()";
+          break;
+
+        default:
+          console.error('Unknown command', command);
+          break;
+      }
     }
 
-    // Apply command
-    for (const tab of tabs) {
-        let code = "";
-
-        if (tab.url.startsWith("https://play.spotify.com")) {
-            code = `document.getElementById('app-player').contentDocument.getElementById('${command}').click()`;
-        } else if (tab.url.startsWith("https://open.spotify.com")) {
-            switch (command) {
-                case "play-pause":
-                    code = "(document.querySelector('.spoticon-play-16') || document.querySelector('.spoticon-pause-16')).click()";
-                    break;
-
-                case "next":
-                    code = "document.querySelector('.spoticon-skip-forward-16').click()";
-                    break;
-
-                case "previous":
-                    code = "document.querySelector('.spoticon-skip-back-16').click()";
-                    break;
-
-                default:
-                    console.error('Unknown command', command);
-                    break;
-            }
-        }
-
-        if (code.length)
-            browser.tabs.executeScript(tab.id, { code: code });
-        else
-            console.error('No code to execute');
-    }
+    if (code.length) browser.tabs.executeScript(tab.id, { code: code });
+    else console.error('No code to execute');
+  }
 }
 
-
 function main() {
-    const ws = new WebSocket('ws://localhost:8079', ['mpris-service']);
+  const ws = new WebSocket('ws://localhost:8079', ['mpris-service']);
 
-    ws.addEventListener('open', () => {
-        ws.send('hello');
-    });
+  ws.addEventListener('open', () => {
+    ws.send('hello');
+  });
 
-    ws.addEventListener('message', event => {
-        console.log(event.data);
-        const json = JSON.parse(event.data);
+  ws.addEventListener('message', event => {
+    console.log(event.data);
+    const json = JSON.parse(event.data);
 
-        switch (json.type) {
-            case 'playpause':
-                runCommand('play-pause');
-                break;
+    switch (json.type) {
+      case 'playpause':
+        runCommand('play-pause');
+        break;
 
-            default:
-                runCommand(json.type);
-                break;
-        }
-
-    });
+      default:
+        runCommand(json.type);
+        break;
+    }
+  });
 }
 
 main();
--- server.js
+++ server.js
@@ -4,81 +4,82 @@ const Player = require('mpris-service');
 
 const port = process.env.PORT || 8079;
 const server = http.createServer((req, res) => {
-    res.writeHead(404);
-    res.rend();
+  res.writeHead(404);
+  res.rend();
 });
 const player = new Player({
-    name: 'spotify-web-player',
-    identity: 'Spotify Web Player ',
-    supportedInterfaces: ['player']
+  name: 'spotify-web-player',
+  identity: 'Spotify Web Player ',
+  supportedInterfaces: ['player'],
 });
 
 server.listen(port, () => {
-    console.log(`Listening to ${port}`);
+  console.log(`Listening to ${port}`);
 });
 
 wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false,
+  httpServer: server,
+  autoAcceptConnections: false,
 });
 
 function isAllowedOrigin(origin) {
-    console.log('isAllowedOrigin', origin);
-    return true;
+  console.log('isAllowedOrigin', origin);
+  return true;
 }
 
 const connections = [];
 
 wsServer.on('request', req => {
-    if (!isAllowedOrigin(req.origin)) {
-        req.reject();
-        return;
+  if (!isAllowedOrigin(req.origin)) {
+    req.reject();
+    return;
+  }
+
+  const conn = req.accept('mpris-service', req.origin);
+  console.log('New Connection accepted.');
+
+  connections.push(conn);
+
+  conn.on('message', msg => {
+    switch (msg.type) {
+      case 'utf8':
+        console.log('Received Message:', msg.utf8Data);
+        // connection.sendUTF(msg.utf8Data);
+        break;
+
+      case 'binary':
+        console.log(
+          'Received Binary Message of',
+          msg.binaryData.length,
+          'bytes'
+        );
+        // connection.sendBytes(msg.binaryData);
+        break;
+
+      default:
+        console.log('Unknown msg.type', msg.type);
+        break;
     }
+  });
 
-    const conn = req.accept('mpris-service', req.origin);
-    console.log('New Connection accepted.');
+  conn.on('close', function(reasonCode, description) {
+    console.log(`Peer ${conn.remoteAddress} disconnected.`);
+    const index = connections.indexOf(conn);
 
-    connections.push(conn);
-
-    conn.on('message', msg => {
-        switch (msg.type) {
-            case 'utf8':
-                console.log('Received Message:', msg.utf8Data);
-                // connection.sendUTF(msg.utf8Data);
-                break;
-
-            case 'binary':
-                console.log('Received Binary Message of', msg.binaryData.length, 'bytes');
-                // connection.sendBytes(msg.binaryData);
-                break;
-
-            default:
-                console.log('Unknown msg.type', msg.type);
-                break;
-
-        }
-    });
-
-    conn.on('close', function(reasonCode, description) {
-        console.log(`Peer ${conn.remoteAddress} disconnected.`);
-        const index = connections.indexOf(conn);
-
-        if (index > -1)
-            connections.splice(index, 1);
-    });
-
+    if (index > -1) connections.splice(index, 1);
+  });
 });
 
 ['next', 'previous', 'pause', 'playpause', 'stop', 'play'].forEach(type => {
-    player.on(type, (...args) => {
-        console.log('Event:', type, args);
-        const msg = JSON.stringify({
-            type,
-            args,
-        });
-
-        connections.forEach(conn => {
-            conn.sendUTF(msg);
-        });
+  player.on(type, (...args) => {
+    console.log('Event:', type, args);
+    const msg = JSON.stringify({
+      type,
+      args,
     });
+
+    connections.forEach(conn => {
+      conn.sendUTF(msg);
+    });
+  });
 });
